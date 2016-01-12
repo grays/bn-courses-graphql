c0b5294da570d1a1739a1f86f0a5b485e3db1986
James Roseman <james.roseman@gmail.com>
2016-01-12 09:27:36 -0500

    Changed App.js

--- js/components/App.js
+++ js/components/App.js
@@ -5,9 +5,9 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Widget list</h1>
+        <h1>Course list</h1>
         <ul>
-          {this.props.viewer.widgets.edges.map(edge =>
+          {this.props.viewer.courses.edges.map(edge =>
             <li key={edge.node.id}>{edge.node.name} (ID: {edge.node.id})</li>
           )}
         </ul>
@@ -20,7 +20,7 @@ export default Relay.createContainer(App, {
   fragments: {
     viewer: () => Relay.QL`
       fragment on User {
-        widgets(first: 10) {
+        courses(first: 10) {
           edges {
             node {
               id,
